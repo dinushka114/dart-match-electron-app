42593fd638c6c572699fd69b3a022acf7c4b29af
Dinushka Piyumal <Dinushka@noritakelk.local>
2025-04-04 16:15:21 +0530

    testtimasdasdasd

--- src/pages/Second/SecondPage.jsx
+++ src/pages/Second/SecondPage.jsx
@@ -9,7 +9,44 @@ const SecondPage = () => {
 
     return (
         <div className='bracket'>
-            
+            <div className="column">
+            {
+                    [0, 1, 2, 3].map(element => {
+                        return <>
+                            <div className="match">
+                                <TeamLeft  team={groups[element][0]} />
+                            </div>
+                            <div className="match">
+                                <TeamLeft  team={groups[element][1]} />
+                            </div>
+                            <div className="match">
+                                <TeamLeft team={groups[element][2]} />
+                            </div>
+                            <div className="match">
+                                <TeamLeft team={groups[element][3]} />
+                            </div></>
+                    })
+                }
+            </div>
+            <div className="column">
+                {
+                    [4, 5, 6, 7].map(element => {
+                        return <>
+                            <div className="match">
+                                <TeamLeft  team={groups[element][0]} />
+                            </div>
+                            <div className="match">
+                                <TeamLeft  team={groups[element][1]} />
+                            </div>
+                            <div className="match">
+                                <TeamLeft team={groups[element][2]} />
+                            </div>
+                            <div className="match">
+                                <TeamLeft team={groups[element][3]} />
+                            </div></>
+                    })
+                }
+            </div>
         </div>
     )
 }
